2874d18efb00ad0c930adcb65cb831e38d11defb
Tavi Manrique <tavidanner96@gmail.com>
2022-12-01 23:25:12 -0500

    Set mongodb_uri

--- src/main/kotlin/com/manriquetavi/di/KoinModule.kt
+++ src/main/kotlin/com/manriquetavi/di/KoinModule.kt
@@ -10,7 +10,7 @@ import org.litote.kmongo.reactivestreams.KMongo
 val koinModule = module {
     single {
         KMongo
-            .createClient()
+            .createClient(System.getenv("MONGODB_URI"))
             .coroutine
             .getDatabase(DATABASE_NAME)
     }
